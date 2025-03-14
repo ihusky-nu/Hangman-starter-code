411bb105b5501fca56fd5d125a7822dd7925720a
nicole-contreras <nicolecontreras9@icloud.com>
2025-03-14 13:57:31 -0400

    Initial Commit

---
+++ HangMan/HangManApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangManApp.swift
+//  HangMan
+//
+//  Created by Nicole on 3/14/25.
+//
+
+import SwiftUI
+
+@main
+struct HangManApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
